e16f692f75369e212a23cd93096774db207cd9fd
luqingyi <744679992@qq.com>
2023-01-11 14:58:23 +0800

    second commit

--- src/main/java/cn/luqingyi/GitTest.java
+++ src/main/java/cn/luqingyi/GitTest.java
@@ -3,5 +3,6 @@ package cn.luqingyi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git1!");
+        System.out.println("Hello git2!");
     }
 }
